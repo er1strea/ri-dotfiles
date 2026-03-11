fd1d48017808446d1a81eabf668eada9a03be62f
Sovr <sovrant@tutamail.com>
2026-03-12 05:20:16 +0700

    Added the files

---
+++ README.md
@@ -0,0 +1,9 @@
+# Ri's Dotfiles
+
+i like teto
+
+## What this dotfiles looks like:
+![image1](images/screenshots/image1.png)
+![image2](images/screenshots/image2.png)
+![image3](images/screenshots/image3.png)
+![image4](images/screenshots/image4.png)
---
+++ scripts/kblo.sh
@@ -0,0 +1,4 @@
+#!/bin/zsh
+
+# hyprctl devices -j | jq -r '.keyboards[] | select(.name == "at-translated-set-2-keyboard") | .active_keymap' | cut -c 1-2 | tr 'A-Z' 'a-z' | sed 's/^en$/us/'
+hyprctl devices -j | jq -r '.keyboards[] | select(.name == "logitech-usb-keyboard") | .active_keymap' | cut -c 1-2 | tr 'A-Z' 'a-z' | sed 's/^en$/us/'
